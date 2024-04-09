3c55dce22b2c113b611e151f917a219a55b68eb9
Frnkp21 <perezcarvajalfrank@elpuig.xeill.net>
2024-04-09 18:50:27 +0200

    1

---
+++ src/main/java/com/example/PracticasPrueba/PracticasPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.example.PracticasPrueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticasPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticasPruebaApplication.class, args);
+	}
+
+}
